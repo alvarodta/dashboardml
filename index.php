2bbc646c1bb8721d36bcfc2d9b5d021462a51aa1
alvarodta <alvarood023@gmail.com>
2022-12-26 16:19:18 -0300

    1.0

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/style.css">
+
+</head>
+
+<body>
+    <div class="main">
+        <div class="esquerda"> 
+            <h1>NOME DO SITE</h1>
+            <button class="b1">Botão 1</button>
+            <button>Botão 2</button>
+            <button>Botão 3</button>
+        </div>
+        <div class="direita">
+            <header class="header-direita">
+                <h2>TÍTULO DA PAGINA</h2>
+            </header>
+            <div class="div-array"></div>
+        </div>
+    </div>
+</body>
+
+</html>
